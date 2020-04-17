15cff191985e9e1292163eca063ee5626f54a844
ips912s29 <60769797+ips912s29@users.noreply.github.com>
2020-04-17 16:39:17 +0700

    Update chessboardInit.h

--- src/chessboardInit.h
+++ src/chessboardInit.h
@@ -1,6 +1,6 @@
 #ifndef chessboardMove
 #define chessboardMove
 
-void chessboardInit(string chessboard[9][9],bool chessboard_b[9][9])
+void chessboardInit(string chessboard[9][9],bool chessboard_b[9][9]);
 
 #endif
